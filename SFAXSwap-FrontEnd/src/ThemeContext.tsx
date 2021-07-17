c33177f5c5c0e2ac888faa25575f459d8d7767f2
addufres <adufresplatinum@gmail.com>
2021-07-17 16:19:26 -0600

    trying to force the text color to show correct.

--- SFAXSwap-FrontEnd/src/ThemeContext.tsx
+++ SFAXSwap-FrontEnd/src/ThemeContext.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { ThemeProvider as SCThemeProvider } from 'styled-components'
-import { light, dark } from './libraries/pancake-uikit/index'
+import { dark } from './libraries/pancake-uikit/index'
 
 const CACHE_KEY = 'IS_DARK'
 
@@ -26,7 +26,7 @@ const ThemeContextProvider: React.FC = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
-      <SCThemeProvider theme={isDark ? dark : dark}>{children}</SCThemeProvider>
+      <SCThemeProvider theme={dark}>{children}</SCThemeProvider>
     </ThemeContext.Provider>
   )
 }
